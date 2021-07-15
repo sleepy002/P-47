abead1533059cae2fcc3f0947be24b552f8b7b10
Ravandel123 <43044835+Ravandel123@users.noreply.github.com>
2021-07-15 20:55:30 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,10 +3,10 @@ module.exports = {
    name: 'ready',
    once: true,
    execute(client) {
-      console.log('Ready!')
+      console.log('Fly away!')
       client.guilds.cache.forEach(guild => console.log('Name: ' + guild.name + ', ID: ' + guild.id))
       client.user.setStatus('available')
-      client.user.setPresence({ activity: { name: 'Drinking Coffee and creating art' }, status: 'online' })
+      client.user.setPresence({ activity: { name: 'Flying' }, status: 'online' })
    },
 };
 
