175a090328acadf114b8e8dab4b43be6994b1ec6
sleepy002 <dinlyvartus@gmail.com>
2021-07-19 15:11:07 -0400

    Create Attack.js

---
+++ commands/Attack.js
@@ -0,0 +1,26 @@
+const commons = require('../usefulmodules/common.js')
+
+module.exports = {
+   name: 'hey',
+   description: 'Attacks.',
+   usage: '[Blows something up] ',
+   example: 'Dinly.',
+   execute(message, args) {
+      
+     
+      
+      
+      
+      // message.channel.send(`This is the message: ${message}`);
+      // message.channel.send(`This is the args: ${args}`);
+      // message.channel.send(`This is the args[0]: ${args[0]}`);
+      // message.channel.send(`This is the args[1]: ${args[1]}`);
+      // message.channel.send(`This is the args[2]: ${args[2]}`);
+      // message.channel.send(`This is the args[3]: ${args[3]}`);
+      
+      // if (!args[1])
+           message.channel.send(`The P-47 dives down and destroys the Axis tank`);
+      // else
+         // message.channel.send(`Attack ${args[1]}!`);
+   },
+}
