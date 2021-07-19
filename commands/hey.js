d1a5f7e09f6d8ef160c8a9fdd5616d751423aed3
Ravandel123 <43044835+Ravandel123@users.noreply.github.com>
2021-07-19 20:06:17 +0200

    Update hey.js

--- commands/hey.js
+++ commands/hey.js
@@ -6,9 +6,12 @@ module.exports = {
    usage: '[someone you want to greet] ',
    example: 'Dinly.',
    execute(message, args) {
-      if (!args[1])
-         message.channel.send(`The plane says nothing because it's a plane`);
-      else
-         message.channel.send(`Hey ${args[1]}!`);
+      
+      message.channel.send(`This is the message: ${message}`);
+      message.channel.send(`This is the args: ${args}`);
+      // if (!args[1])
+         // message.channel.send(`The plane says nothing because it's a plane`);
+      // else
+         // message.channel.send(`Hey ${args[1]}!`);
    },
 }
